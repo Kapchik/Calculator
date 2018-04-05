1f7411a5552aa7373509900c94332012c6b20b88
Kapchik <ra650314@gmail.com>
2018-04-06 00:12:29 +0300

    commit

--- MyCalc/src/com/company/Calculator.java
+++ MyCalc/src/com/company/Calculator.java
@@ -2,24 +2,27 @@ package com.company;
 
 import java.util.*;
 
-//можно еще перейти от int к дробным, добавить скобки и обработать exception (del 0), добавить ввод значений руками
-//но в первом приближении, я считаю, готово
+//можно еще перейти от int к дробным, добавить скобки и обработать exception (del 0), добавить ввод значений руками,
+//прописать в явном виде equals, сравнивать через него
+//но во втором уже))) приближении, я считаю, готово
 
 public class Calculator {
 
-    public static final int DEFUALT_VALUE = 25;
+    private static final int DEFUALT_VALUE = 25;
+    private static final int MAX_VALUE = 100;
+    private static final int MAX_SIZE_EXPRESSION = 10;
 
     public static void main(String[] args) {
         int vnVar;
         Random vnRand = new Random();
-        ArrayList<String> vaArray = new ArrayList<String>();
+        ArrayList<String> vaArray = new ArrayList<>();
 
-        for(int i = 0; i < 10; i++) {
+        for(int i = 0; i < MAX_SIZE_EXPRESSION; i++) {
 
-            int j = vnRand.nextInt(98) + 1;
+            int j = vnRand.nextInt(MAX_VALUE) + 1;
             vaArray.add(String.valueOf(j));
 
-            if (i != 9) {
+            if (i != MAX_SIZE_EXPRESSION - 1) {
                 vnVar = j / DEFUALT_VALUE;
 
                 switch (vnVar) {
@@ -37,11 +40,15 @@ public class Calculator {
 
         for(int i = 0; i < vaArray.size(); i++) System.out.print(vaArray.get(i) + " ");
 
-        MathOperations.multiply(vaArray);
+        MathOperations math = new MathOperations();
 
-        MathOperations.divide(vaArray);
-
-        MathOperations.addandsubstract(vaArray);
+        for (int i = 0; i < Actions.values().length; i++) {
+            for (int j = 1; j < vaArray.size(); j += 2) {
+                if (vaArray.get(j) == Actions.values()[i].getAct()) {
+                    math.execute(j, vaArray);
+                }
+            }
+        }
 
         System.out.print("= " + vaArray.get(0));
 
